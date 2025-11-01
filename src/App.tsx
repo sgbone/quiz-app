21370833f01e6ddbc61fa90b073a4ba167b0c54f
sgbone <nhaccuaducanh@gmail.com>
2025-11-01 18:52:51 +0700

    Fix reset password

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { useEffect } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import { useAppStore } from "./store/quizStore";
 import { useAuthStore } from "./store/authStore"; // Import auth store
-import { supabase } from "./supabaseClient"; // Import supabase client
+import { supabase } from "./supabaseClient";
+import { Session } from "@supabase/supabase-js"; // Import supabase client
 
 // Import tất cả các trang
 import WelcomePage from "./pages/WelcomePage";
@@ -34,7 +35,7 @@ function App() {
     });
 
     const { data: authListener } = supabase.auth.onAuthStateChange(
-      (_event, session) => {
+      (_event: string, session: Session | null) => {
         setSession(session);
         if (session) {
           fetchProfile();
